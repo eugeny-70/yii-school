3207251c4d62e73faf3ad64688b46f32d54a3977
Eugeny Khotskiy <eugeny.khockij@gmail.com>
2019-10-10 11:32:11 +0300

    add debug line

--- config/web.php
+++ config/web.php
@@ -2,10 +2,21 @@
    return [
     'id' => 'video',
      'basePath' => realpath(__DIR__ . '/../'),
+       'bootstrap' => ['debug'],
        'components' => [
            'urlManager' =>[
                'enablePrettyUrl' => true,
                'showScriptName' => false
+           ],
+           'request' => [
+               'cookieValidationKey' => '151565+652'
            ]
+       ],
+       'modules' => [
+           'debug' => [
+              'class' => 'yii\debug\Module',
+               'allowedIPs' => ['127.0.0.1', '::1']
+               ]
+
        ]
    ];
--- web/index.php
+++ web/index.php
@@ -1,6 +1,7 @@
 <?php
   define('YII_DEBUG',true);
   require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
+  require __DIR__ . '/../vendor/autoload.php';
   $config = require __DIR__ . '/../config/web.php';
   $yii = new yii\web\Application($config);
   $yii->run();
