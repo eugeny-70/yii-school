f9272d0f76b593a1b9ed41d0ccbf7362eb32d3a3
Eugeny Khotskiy <eugeny.khockij@gmail.com>
2019-10-10 16:54:56 +0300

    add database connection

---
+++ config/db.php
@@ -0,0 +1,8 @@
+<?php
+  return[
+      'class' => 'yii\db\Connection',
+      'dsn' => 'pgsql:host=localhost;port=5432;dbname=school',
+      'username' => 'postgres',
+      'password' => 'pgadmin',
+      'charset' => 'utf8',
+  ];
