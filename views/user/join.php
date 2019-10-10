da7934664080171d9eb1bec96f7c7eae849791e4
Eugeny Khotskiy <eugeny.khockij@gmail.com>
2019-10-10 11:55:57 +0300

    describe join and login pages

--- views/user/join.php
+++ views/user/join.php
@@ -1,1 +1,14 @@
-<h1>Join us</h1>
+<div class="panel panel-warning">
+  <div class="panel-heading">
+      <h1>Join us</h1>
+  </div>
+   <div class="panel-body">
+       name<br/>
+       email<br/>
+       password<br/>
+       password2<br/>
+       [Create]
+   </div>
+</div>
+
+
